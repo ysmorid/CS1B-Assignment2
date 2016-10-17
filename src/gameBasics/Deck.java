14a4835f092b8938e629b09b263a60635805fa58
Ylia Morid <ylsmo15@gmail.com>
2016-10-17 03:34:26 -0700

    Deck: added variables

--- src/gameBasics/Deck.java
+++ src/gameBasics/Deck.java
@@ -1,5 +1,17 @@
 package gameBasics;
 
 public class Deck {
+   public final int MAX_CARDS = 312;
+   private static Card[] masterPack;
+   private Card[] cards;
+   private int topCard;
+   private int numPacks;
 
+   Deck(){
+      
+   }
+   
+   Deck(int numPacks){
+      
+   }
 }
