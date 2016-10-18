c1ce98b74701ca7befb000791c05be437252d408
Ylia Morid <ylsmo15@gmail.com>
2016-10-17 17:38:04 -0700

    Deck - setting up constructors

--- src/gameBasics/Deck.java
+++ src/gameBasics/Deck.java
@@ -2,16 +2,23 @@ package gameBasics;
 
 public class Deck {
    public final int MAX_CARDS = 312;
-   private static Card[] masterPack;
+   private static Card[] masterPack = new Card[52];
    private Card[] cards;
    private int topCard;
    private int numPacks;
 
-   Deck(){
+   Deck(){ 
       
-   }
+       }
    
    Deck(int numPacks){
       
+   }
+   
+   private static void allocateMasterPack(){
+      for(int i = 0; i <= masterPack.length - 1; i++){
+         //loop create cards with enum type, switch to next enum when full
+            //innner loop to create cards 2-k within enum type
+      }
    }
 }
