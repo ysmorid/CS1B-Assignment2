d1bcec7b98d6be53693a219d294a78d4484c9910
Ylia Morid <ylsmo15@gmail.com>
2016-10-18 01:12:38 -0700

    allocateMasterPack() method set up to populate masterPack[]
    Deck() default constructor set up to run allocateMasterPack()

--- src/gameBasics/Deck.java
+++ src/gameBasics/Deck.java
@@ -1,5 +1,7 @@
 package gameBasics;
 
+import gameBasics.Card.Suit;
+
 public class Deck {
    public final int MAX_CARDS = 312;
    private static Card[] masterPack = new Card[52];
@@ -8,7 +10,7 @@ public class Deck {
    private int numPacks;
 
    Deck(){ 
-      
+      allocateMasterPack();
        }
    
    Deck(int numPacks){
@@ -16,9 +18,43 @@ public class Deck {
    }
    
    private static void allocateMasterPack(){
+      char value;
+      Suit suit;
+      
       for(int i = 0; i <= masterPack.length - 1; i++){
-         //loop create cards with enum type, switch to next enum when full
-            //innner loop to create cards 2-k within enum type
+         masterPack[i] = new Card();
+      }  
+      
+      for(int j = 0; j < 4; j++){
+         switch(j){
+         case 0:
+            suit = Card.Suit.CLUBS;
+            break;
+         case 1:
+            suit = Card.Suit.DIAMONDS;
+            break;
+         case 2:
+            suit = Card.Suit.HEARTS;
+            break;
+         case 3:
+            suit = Card.Suit.SPADES;
+            break;
+         default:
+            suit = Card.Suit.SPADES;
+           break;
+         }
+         
+         masterPack[13*j].setCard('A', suit);
+         int k = 1;
+         for(value = '2'; value >= '2' && value <= '9'; value++){
+            masterPack[13*j + k].setCard(value, suit);
+            k++;
+         }
+         masterPack[13*j+9].setCard('T', suit);
+         masterPack[13*j+10].setCard('J', suit);
+         masterPack[13*j+11].setCard('Q', suit);
+         masterPack[13*j+12].setCard('K', suit);
       }
    }
+   
 }
