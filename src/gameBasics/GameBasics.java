c7a7b4fccb4ddde16503f72ecc677753bc4ac938
Ylia Morid <ylsmo15@gmail.com>
2016-10-18 20:49:58 -0700

    dealToHands() method deals cards from Deck object to Hand objects in Hands[] array.
    getNumHands() method asks user how many hands they are playing, tests
    for valid response, then returns int input
    
    display() method displays all the cards per hand object in Hands[] array

--- src/gameBasics/GameBasics.java
+++ src/gameBasics/GameBasics.java
@@ -11,6 +11,10 @@ public class GameBasics {
       Deck deck3 = new Deck();
       Deck deck4;
       Hand[] hands;
+      int userInput;
+      
+      System.out.println("------------------RUN 1----------------");
+      System.out.println();
       
       deck1.display();
       System.out.println();
@@ -26,24 +30,9 @@ public class GameBasics {
       deck3.display();
       
       System.out.println("------------------RUN 2----------------");
+      System.out.println();
       
-      int userInput = 0;
-      Scanner keyboard = new Scanner(System.in);
-      boolean test = false;
-      
-      while(!test){
-         System.out.println("How many hands would you like to play? You can choose"
-               + " between 1 - 10:");
-         userInput = keyboard.nextInt();
-         if(userInput < 1 || userInput > 10){
-            System.out.println("I'm sorry, that is not a valid amount. Please try again.");
-         }
-         else{
-            test = true;
-            keyboard.close();
-         }   
-      }
-      
+      userInput = getNumHands();
       deck4 = new Deck();
       hands = new Hand[userInput];
       
@@ -52,17 +41,54 @@ public class GameBasics {
          hands[i] = hand;
       }
       
-      while(deck4.getTopCard() > 0){
-         for(int k = 0; k < userInput; k++){
-            if(deck4.getTopCard() > 0){
-               hands[k].takeCard(deck4.dealCard());
+      dealToHands(deck4, hands, userInput);
+      display(userInput, hands);
+     
+      for(int i = 0; i < userInput; i++){
+         hands[i].resetHand();
+      }
+      
+      deck4 = new Deck();
+      deck4.shuffle();
+      
+      dealToHands(deck4, hands, userInput);
+      display(userInput, hands);
+   }
+   
+   public static void dealToHands(Deck deck, Hand[] hand, int input){
+      while(deck.getTopCard() > 0){
+         for(int k = 0; k < input; k++){
+            if(deck.getTopCard() > 0){
+               hand[k].takeCard(deck.dealCard());
             }
          }
       }
-      for (int i = 0; i < userInput; i++){
-         System.out.println("Hand: " + hands[i].toString());
+   }
+   
+   public static void display(int input, Hand[] hand){
+      for (int i = 0; i < input; i++){
+         System.out.println("Hand: " + hand[i].toString());
          System.out.println();
       }
-     
+   }
+   
+   public static int getNumHands(){
+      int input = 0;
+      Scanner keyboard = new Scanner(System.in);
+      boolean test = false;
+      
+      while(!test){
+         System.out.println("How many hands would you like to play? You can choose"
+               + " between 1 - 10:");
+         input = keyboard.nextInt();
+         if(input < 1 || input > 10){
+            System.out.println("I'm sorry, that is not a valid amount. Please try again.");
+         }
+         else{
+            test = true;
+            keyboard.close();
+         }   
+      }
+      return input;
    }
 }
